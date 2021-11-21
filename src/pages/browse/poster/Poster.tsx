d7aad3359175d26cc4a803c07e6f128d63f31e8e
Marcela F <54597344+marcelala@users.noreply.github.com>
2021-11-21 20:02:38 +0100

    nav fixes

--- src/pages/browse/poster/Poster.tsx
+++ src/pages/browse/poster/Poster.tsx
@@ -12,12 +12,14 @@ export default function Poster({ media }: iProps) {
   const baseURL = "https://image.tmdb.org/t/p/original";
   const posterObject = `${baseURL}${media.backdrop_path}`;
   const [openModal, setModalOpen] = useState(false);
+  const title = media.title || media.name || media.original_name;
 
   return (
     <li className="row-poster">
       <button onClick={() => setModalOpen(true)}>
-        <img src={posterObject || fallback} alt={media.title} />
+        <img src={posterObject || fallback} alt={title} />
       </button>
+      <span>{title}</span>
       <Modal openModal={openModal} setModalOpen={setModalOpen}>
         <VideoModal media={media} />
       </Modal>
